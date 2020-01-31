29afc729653912fa9fa3d2c6ddeffda31bf5af32
francisco <Fpachecop0@gmail.com>
2020-01-31 17:28:24 -0600

    search

--- Screens/SearchScreen.js
+++ Screens/SearchScreen.js
@@ -7,21 +7,74 @@
  */
 
 import React ,{Component} from 'react';
-import {SafeAreaView,StyleSheet,View,StatusBar,Image,Text} from 'react-native';
-
+import {SafeAreaView,StyleSheet,View,Text} from 'react-native';
+import { Container, Header, Item, Input, Icon} from 'native-base'
+import SearchList from '../components/ListView/ListViewSearchGames'
 
 
 
 class SearchScreen extends Component {
-  state = {}
+  constructor(props) {
+    super(props);
+  }
+  state = {
+    search: '',
+    loading: false,      
+    data: [],
+    extra:[],
+  };
+
+
+  componentDidMount() {
+    return fetch('https://tarea-api-u.herokuapp.com/games')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      var newArray = [];
+      for (let index = 0; index < responseJson.length; index++) {
+        newArray = newArray.concat(responseJson[index].results);
+      }
+
+      this.setState({
+        data: newArray,
+        extra:newArray,
+        isLoading:true,
+      })
+    })
+    .catch((error) =>{
+      console.error(error);
+    });
+  }
+
+
   render() {
+
+    searchFilterFunction = text => {
+      this.setState({
+        search: text,
+      });
+  
+      const newData = this.state.extra.filter(item => {
+        const itemData = `${item.name.toUpperCase()}`;
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      this.setState({
+        data: newData,
+      });
+    };
+
+
     return (
-        <SafeAreaView>
           <View>
-            <Text>Search</Text>
+            <Header searchBar rounded>
+          <Item>
+            <Icon name="ios-search" />
+            <Input placeholder="Search" onChangeText={text => searchFilterFunction(text)} />
+          </Item>
+        </Header>
+          <SearchList isLoading={this.state.loading} gamesList={this.state.data} navigation={this.props.navigation}/>
           </View>
-          </SafeAreaView>
-          
+  
           );
   }
 }
@@ -35,10 +88,9 @@ const styles = StyleSheet.create({
     justifyContent:"center",
     alignItems:"center",
   },
-  logo:{
-    marginTop:"-75%",
-    width: "50%", 
-    height:"40%",
+  spiner:{
+    justifyContent:"center",
+    alignItems:"center"
   }
 });
 
---
+++ components/ListView/ListViewSearchGames.js
@@ -0,0 +1,78 @@
+import React ,{Component} from 'react';
+import {SafeAreaView,StyleSheet,View,ScrollView,Text,Image} from 'react-native';
+import { Spinner} from 'native-base';
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
+
+
+class ListViewSearchGames extends Component {
+  state = {
+  }
+  componentDidMount(){
+  }
+
+
+  render() {
+    const renderGridItems = (data) =>{
+      return(
+        <TouchableOpacity style={styles.gridItem} onPress={()=>{
+          this.props.navigation.navigate({
+                                          routeName:'Game',
+                                          params:{
+                                            GameData: data.item
+                                          }});
+        }}>
+        <View>
+        <Image
+          style={styles.Image}
+          source={{uri: data.item.background_image}}
+          />
+          <Text style={styles.text}>{data.item.name}</Text>
+        </View>
+        </TouchableOpacity>
+      );
+    }
+
+    return (
+        <SafeAreaView>
+          <View>
+            <FlatList keyExtractor={(item,index)=> item.id} data={this.props.gamesList} renderItem={renderGridItems} numColumns={1}></FlatList>
+          </View>
+        </SafeAreaView>
+          
+          );
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+
+  gridItem:{
+    flex:1,
+    margin:15,
+    height:200,
+    width:150,
+    backgroundColor:"#f95959",
+    borderRadius:10,
+    shadowColor: "white",
+    shadowOffset: {
+	    width: 0,
+	    height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+  },
+  Image:{
+    width: 150,
+    height: 150,
+  },
+  text:{
+    marginTop:10,
+    color:"white",
+    textAlign:"center"
+  }
+
+});
+
+export default ListViewSearchGames;
